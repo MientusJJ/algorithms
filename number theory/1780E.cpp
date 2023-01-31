f2831dbb7ff4df42fa7b54476dd143bf6d3c56df
MientusJJ <60322638+MientusJJ@users.noreply.github.com>
2023-01-31 16:24:27 +0100

    Create 1780E.cpp

---
+++ number theory/1780E.cpp
@@ -0,0 +1,82 @@
+#ifdef _MSC_VER
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+//#define DEBUG
+
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <cstring>
+#include <iomanip>
+#include <numeric>
+#include <string>
+#include <cmath>
+#include <math.h>
+#include <set>
+#include <map>
+#include <stack>
+#include <bitset>
+#include <fstream>
+#include <chrono>
+#include <random>
+#include <fstream>
+#define es ios::sync_with_stdio(false);cin.tie(nullptr);
+//#define x first
+//#define y second
+#define all(v) (v).begin(),(v).end()
+#define prec(i) cout << std::fixed << std::setprecision(i)
+#define randomizer mt19937 rng(chrono::steady_clock::now().time_since_epoch().count())
+#define mst(v,k) memset((v),(k),sizeof(v));
+
+
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using pdd = pair<double, double>;
+const int maxik = 1e5 + 7;
+const int maxN = 30;
+const int INF = 0x3f3f3f3f;
+const ll INFll = 1e18;
+const int modik = 1e9 + 7;
+const int tight = 2;
+const int hash2 = 11777;
+const int hash1 = 12269;
+const double eps = 1e-6;
+const int modik2 = 998'244'353;
+const int generatorModik2 = 3;
+const double PI = acos(-1.);
+#ifdef DEBUG
+fstream in("C:/Temp/input.txt", ios::in);
+fstream out("C:/Temp/output.txt", ios::out | ios::trunc);
+#define cin in
+#define cout out
+#endif // DEBUG 
+
+int t;
+void solve()
+{
+	ll L, R;
+	cin >> L >> R;
+	ll ans{ max(0ll,R / 2 - L + 1) };
+	for(ll left=1,right;left<L;left=right+1)
+	{
+		ll f{ (L + left - 1) / left };
+		right=(L + f - 2) / (f - 1) - 1;
+		ans += max(0ll, min(right, R / (f + 1)) - left + 1);
+	}
+	cout << ans << endl;
+}
+int main()
+{
+	es;
+	cin >> t;
+	while(t--)
+	{
+		solve();
+	}
+	return 0;
+}
